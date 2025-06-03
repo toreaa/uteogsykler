67e59a0e0a23319dde4955acdf98bd3f475fe9e5
Torelaa <tabodsvik@gmail.com>
2025-06-03 21:40:13 +0200

    Create __init__.py

---
+++ src/pages/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pages module for Konkurranseapp
+Each page is a separate module for better organization
+"""
+
+from .leaderboard import show_leaderboard_page
+
+__all__ = [
+    'show_leaderboard_page'
+]
