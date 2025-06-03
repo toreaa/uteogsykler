4780c9856bc3f1fc857f3d6be6a6367ead8b24ad
Torelaa <tabodsvik@gmail.com>
2025-06-03 21:44:38 +0200

    Update __init__.py

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -4,7 +4,9 @@ Each page is a separate module for better organization
 """
 
 from .leaderboard import show_leaderboard_page
+from .activities import show_activities_page
 
 __all__ = [
-    'show_leaderboard_page'
+    'show_leaderboard_page',
+    'show_activities_page'
 ]
