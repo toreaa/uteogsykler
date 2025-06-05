045199b3bd36006768066122653b85482d5031ee
Torelaa <tabodsvik@gmail.com>
2025-06-05 02:03:51 +0200

    Update __init__.py

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -2,12 +2,12 @@
 Pages module for Konkurranseapp
 Each page is a separate module for better organization
 """
-
 from .leaderboard import show_leaderboard_page
 from .activities import show_activities_page
 from .dashboard import show_dashboard_page
 from .profile import show_profile_page
 from .admin import show_admin_page
+from .admin_activities import show_admin_activities_page
 
 # Import system admin page with try/except for security
 try:
@@ -21,5 +21,6 @@ __all__ = [
     'show_dashboard_page',
     'show_profile_page',
     'show_admin_page',
+    'show_admin_activities_page',
     'show_analytics_page'
 ]
